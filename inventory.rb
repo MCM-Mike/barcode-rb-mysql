317657b0d0fcdc6efb2dd7a82c7e855ec7b5c081
Casey Scarborough <caseyscarborough@gmail.com>
2013-04-06 23:42:22 -0400

    Added insert and delete functionality

--- inventory.rb
+++ inventory.rb
@@ -2,10 +2,14 @@
 require 'rubygems'
 require 'mysql'
 
-# connection = File.read("connection.txt").split(",").map(&:strip)
+$db = 'localhost'
+$db_user = 'root'
+$db_pass = ''
+$db_name = 'inventory'
+
 def open_connection
 	begin
-		db = Mysql.new('localhost', 'root', '', 'inventory')
+		db = Mysql.new($db, $db_user, $db_pass, $db_name)
 	rescue Mysql::Error
 		puts "Oops! Couldn't connect to database!"
 	end
@@ -13,36 +17,10 @@ def open_connection
 	return db
 end
 
-def display_help
-	# define the help variable which holds the help text
-	help = "Usage: ruby inventory.rb [?|-h|help|[-u|-o|-z <infile>|[<outfile>]]]\nParameters:
-   ?                     displays this usage information
-   -h                    displays this usage information
-   help                  displays this usage information
-   -u <infile>           update the inventory using the file <infile>.
-                         The filename <infile> must have a .csv
-                         extension and it must be a text file in comma
-                         separated value (CSV) format. Note that the
-                         values must be in double quote.
-   -z|-o [<outfile>]     output either the entire content of the
-                         database (-o) or only those records for which
-                         the quantity is zero (-z). If no <outfile> is
-                         specified then output on the console otherwise
-                         output in the text file named <outfile>. The
-                         output in both cases must be in a tab separated
-                         value (tsv) format."
-	# print the help screen
-	puts help
-end # display_help
-
-db = open_connection()
-# if argument is ?, -h, or help, display the help screen
-if (ARGV[0] == "?" or ARGV[0] == "-h" or ARGV[0] == "help")
-	display_help
-# if it is -u, call the update_inventory function
-elsif (ARGV[0] == "-o")
+def display_contents
+	db = open_connection()
 	begin
-		results = db.query "SELECT * FROM inventory"
+		results = db.query "SELECT * FROM #{$db_name}"
 		puts "Number of items: #{results.num_rows}"
 		results.each_hash do |item|
 			puts "Barcode: #{item['Barcode']}"
@@ -58,3 +36,68 @@ elsif (ARGV[0] == "-o")
 		db.close
 	end
 end
+
+def add_information
+	db = open_connection()
+	print "Enter Barcode: "
+	barcode = gets.strip
+	print "Enter Item Name: "
+	item_name = gets.strip
+	print "Item Category: "
+	item_category = gets.strip
+	print "Quantity: "
+	quantity = gets.strip
+	print "Price: "
+	price = gets.strip
+	print "Description: "
+	description = gets.strip
+	begin
+		insert_new_item = db.prepare "INSERT INTO #{$db_name} (Barcode, ItemName, ItemCategory, Quantity, Price, Description) VALUES (?, ?, ?, ?, ?, ?)"
+		insert_new_item.execute barcode,item_name,item_category,quantity,price,description
+		insert_new_item.close
+	rescue
+		abort "There was an error adding information into the database."
+	ensure
+		db.close
+	end
+	puts "Information added successfully!"
+end
+
+def delete_information
+	db = open_connection()
+	print "Enter the barcode for the item you'd like to delete: "
+	barcode = gets.strip
+	begin
+		delete_item = db.prepare "DELETE FROM #{$db_name} WHERE Barcode = ?"
+		delete_item.execute barcode
+		delete_item.close
+	rescue
+		abort "There was an error deleting item from the database. Please ensure that item exists."
+	ensure
+		db.close
+	end
+	puts "Information deleted successfully!"
+
+end
+
+def home_screen
+	puts "Welcome to the Ruby/MySQL Inventory Application!"
+	puts "What would you like to do?"
+	puts "1: List Database Contents"
+	puts "2: Enter New Item Into Database"
+	puts "3: Remove Item From Database"
+	user_choice = gets.strip
+	case user_choice
+	when "1"
+		display_contents
+	when "2"
+		add_information
+	when "3"
+		delete_information
+	else
+		puts "You entered an incorrect option."
+	end
+end
+
+
+home_screen()
