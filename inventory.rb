50ccd83dcc02ecb1dfb0ae95fc3c3413309daca2
Casey Scarborough <caseyscarborough@gmail.com>
2013-04-07 15:18:09 -0400

    Added database creation and comments

--- inventory.rb
+++ inventory.rb
@@ -12,12 +12,12 @@ $sample_data_filename = "./sample_data.csv"
 # $db_user = 'root'
 # $db_pass = 'root'
 # $db_name = 'inventory'
-# $db_table_name = 'hello'
+# $db_table_name = 'inventory'
 
 # This function opens a connection to the database
 def open_connection
 	begin
-		db = Mysql.new($db_hostname, $db_user, $db_pass, $db_name)
+		db = Mysql.new($db_hostname, $db_user, $db_pass)
 	rescue Mysql::Error
 		abort "Oops! Couldn't connect to database! Make sure you entered the correct information."
 	end
@@ -30,7 +30,7 @@ def display_contents
 	db = open_connection
 	begin
 		# Set results equal to the result of the MySQL query
-		results = db.query "SELECT * FROM #{$db_table_name}"
+		results = db.query "SELECT * FROM #{$db_name}.#{$db_table_name}"
 		puts "Number of items: #{results.num_rows}"
 
 		puts "+----------------+---------------------------------+-----------------+----------+---------+-----------------------------+"
@@ -78,7 +78,7 @@ def add_information
 	description = gets.strip
 
 	begin # Prepare database query and execute it
-		insert_new_item = db.prepare "INSERT INTO #{$db_table_name} (Barcode, ItemName, ItemCategory, Quantity, Price, Description) VALUES (?, ?, ?, ?, ?, ?)"
+		insert_new_item = db.prepare "INSERT INTO #{$db_name}.#{$db_table_name} (Barcode, ItemName, ItemCategory, Quantity, Price, Description) VALUES (?, ?, ?, ?, ?, ?)"
 		insert_new_item.execute barcode,item_name,item_category,quantity,price,description
 		insert_new_item.close
 	rescue # Catch any errors
@@ -98,7 +98,7 @@ def delete_information
 	print "Enter the barcode for the item you'd like to delete: "
 	barcode = gets.strip
 	begin # Check if barcode exists
-		delete_item = db.prepare "DELETE FROM #{$db_table_name} WHERE Barcode = ?"
+		delete_item = db.prepare "DELETE FROM #{$db_name}.#{$db_table_name} WHERE Barcode = ?"
 		delete_item.execute barcode
 		delete_item.close
 	rescue # Catch any errors
@@ -117,7 +117,7 @@ def update_information
 	print "Enter the barcode for the item you'd like to update: "
 	barcode = gets.strip
 	begin # Check if barcode exists
-		check_item = db.query "SELECT * FROM #{$db_table_name} WHERE Barcode = #{barcode} LIMIT 1"
+		check_item = db.query "SELECT * FROM #{$db_name}.#{$db_table_name} WHERE Barcode = #{barcode} LIMIT 1"
 		if (check_item.num_rows == 1)
 			begin # Prepare database query and execute it
 				column = ""
@@ -164,10 +164,10 @@ def update_information
 					puts "You did not enter a correct option."
 				end
 				if (column != "" && value != "")
-					update_item = db.prepare "UPDATE #{$db_table_name} SET #{column} = ? WHERE Barcode = #{barcode}"
+					update_item = db.prepare "UPDATE #{$db_name}.#{$db_table_name} SET #{column} = ? WHERE Barcode = #{barcode}"
 					update_item.execute value
 					update_item.close
-					check_item = db.query "SELECT * FROM #{$db_table_name} WHERE Barcode = #{barcode} LIMIT 1"
+					check_item = db.query "SELECT * FROM #{$db_name}.#{$db_table_name} WHERE Barcode = #{barcode} LIMIT 1"
 					print "\n"
 					check_item.each_hash do |item|
 						puts "Barcode: #{item['Barcode']}"
@@ -223,6 +223,7 @@ def home_screen
 	end
 end
 
+# This function loads the sample data from the global $sample_data_filename
 def load_sample_data
 	begin # try to load the file
 		# open the file and create an array to hold it
@@ -241,28 +242,63 @@ def load_sample_data
 end
 
 
+
+#------- MAIN APPLICATION -------#
+
+
 puts "Welcome to the Ruby/MySQL Inventory Application!"
+
+# Prompt for database information
 print "Enter Database Hostname: "
 $db_hostname = gets.strip
 print "Enter Username: "
 $db_user = gets.strip
-begin
+begin # Use highline gem to blank password if available
 	$db_pass = ask("Enter password: ") { |q| q.echo = false}
-rescue
+rescue # If not, just show password
 	print "Enter Password: "
 	$db_pass = gets
 end
-print "Enter Database Name: "
-$db_name = gets.strip
 db = open_connection
 puts "Database connection successful!"
+print "Enter Database Name: "
+$db_name = gets.strip
+
+# Check if the database exists, and ask the user if they'd like to create it if it doesn't
+check_database = db.query "SHOW DATABASES LIKE '#{$db_name}'"
+if (check_database.num_rows > 0)
+	# Use the database if it exists
+	db.query "USE #{$db_name}"
+else
+	user_choice = ""
+	loop do # Ask the user if they'd like to create the database
+		print "Database does not exist, would you like to create it? (requires permissions) Y/N: "
+		user_choice = gets.strip.upcase
+		break if (user_choice == "Y" || user_choice == "N")
+	end
+	if (user_choice == "Y")
+		begin # If yes, try to create the database and select it
+			db.query "CREATE DATABASE #{$db_name}"
+			db.query "USE #{$db_name}"
+		rescue # catch any errors
+			abort "An error has occurred. Unable to create database"
+		end
+		puts "Database successfully created!"
+	elsif (user_choice == "N")
+		abort "Goodbye!" # Exit the program
+	end
+end
+
+# Prompt for table name
 print "Enter the name of your table: "
 $db_table_name = gets.strip
+
+# Check if table exists
 check_table = db.query "SHOW TABLES LIKE '#{$db_table_name}'"
 if (check_table.num_rows > 0)
-	db.close
+	db.close # If it does, close connection and open the home screen
 	home_screen
-else
+else # If it does not, ask the user if they'd like to create it
 	user_choice = ""
 	loop do
 		print "Table does not exist in database, would you like to create it? (requires permissions) Y/N: "
@@ -270,7 +306,7 @@ else
 		break if (user_choice == "Y" || user_choice == "N")
 	end
 	if (user_choice == "Y")
-		begin
+		begin # If yes, create the table with the appropriate columns
 			db.query("CREATE TABLE IF NOT EXISTS #{$db_table_name} ( \
 				Barcode varchar(20) NOT NULL, \
 				ItemName varchar(40) NOT NULL, \
@@ -284,27 +320,27 @@ else
 			puts e
 		end
 		puts "Table successfully created!"
-		loop do
+		loop do # Ask the user if they'd like to populate the table
 			print "Would you like to populate with sample data? (requires permissions) Y/N: "
 			user_choice = gets.strip.upcase
 			break if (user_choice == "Y" || user_choice == "N")
 		end
-		if (user_choice == "Y")
+		if (user_choice == "Y") # If yes, load the data and populate the table
 			sample_data = load_sample_data
-			begin
+			begin # Loop through the data and insert each row
 			sample_data.each do |a|
 				db.query("INSERT INTO #{$db_table_name} ( \
 					`Barcode`, `ItemName`, `ItemCategory`, `Quantity`, `Price`, `Description` \
 					) VALUES ( \
 					'#{a[0]}', '#{a[1]}', '#{a[2]}', #{a[3]}, #{a[4]}, '#{a[5]}')")
 			end
-			rescue Mysql::Error => e
+			rescue Mysql::Error => e # Catch any errors
 				puts "Could not populate table."
 				puts e
 			end
 		end
 		puts "Table populated successfully!"
-		db.close
+		db.close # Close the connection and display home screen
 		home_screen
 	elsif (user_choice == "N")
 		abort "Goodbye!"
